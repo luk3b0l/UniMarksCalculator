f70c21b8c01bb06727d082a6f125a698a234853d
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-13 21:41:24 +0100

    Added initial version of the marksGUI (programmed manually, not using the Netbeans GUI Builder). Added initial version of the menu. This class has been created as the marksGUItest class uses GUI builder which produces a generated code, thus less maintainable in the future.

--- UniMarksCalculator/src/unimarkscalculator/marksGUI.java
+++ UniMarksCalculator/src/unimarkscalculator/marksGUI.java
@@ -24,7 +24,76 @@ public class marksGUI
     
     public marksGUI()
     {
-        
+        setFrame();
     }
     
+    private void setFrame()
+    {
+        Container contentPane = myFrame.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        Dimension preferredSize = new Dimension(600, 400);
+        myFrame.setPreferredSize(preferredSize);
+        setMenuBar(myFrame);
+        
+        myFrame.pack();
+        myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setLocationRelativeTo(null);    // setting the program in the centre of the screen
+    }
+
+    private void setMenuBar(JFrame frame)
+    {
+        JMenuBar menubar = new JMenuBar();
+        frame.setJMenuBar(menubar);
+        
+        // *** FILE menu:
+        JMenu fileMenu = new JMenu("File");
+        menubar.add(fileMenu);
+        
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(exitItem);
+        //exitItem.addActionListener(new ExitHandler());                           // TESTING!
+        // -----------------------------------------------
+        
+        // *** PROFILE menu:
+        JMenu profileMenu = new JMenu("Profile");
+        menubar.add(profileMenu);
+        
+        JMenuItem newItem = new JMenuItem("New");
+        profileMenu.add(newItem);
+        // TODO - add Action Listener
+
+        JMenuItem loadItem = new JMenuItem("Load");
+        profileMenu.add(loadItem);
+        // TODO - add Action Listener
+
+        JMenuItem editItem = new JMenuItem("Edit");
+        profileMenu.add(editItem);
+        // TODO - add Action Listener  
+ 
+        JMenuItem deleteItem = new JMenuItem("Delete");
+        profileMenu.add(deleteItem);
+        // TODO - add Action Listener
+        // -----------------------------------------------
+        
+
+        
+        // *** HELP menu:
+        JMenu helpMenu = new JMenu("Help");
+        menubar.add(helpMenu);
+        
+        JMenuItem howToItem = new JMenuItem("How to use");
+        helpMenu.add(howToItem);
+        //howToItem.addActionListener(new howToUseHandler());
+        
+        JMenuItem aboutItem = new JMenuItem("About");
+        helpMenu.add(aboutItem);
+        //aboutItem.addActionListener(new aboutHandler());
+        // -----------------------------------------------
+    }
+    
+    public static void main(String[] args)
+    {
+        new marksGUI();
+    }
 }
