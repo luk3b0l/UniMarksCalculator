bb4e974852c792e48201bd0e69402d3137693c56
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-26 23:31:36 +0100

    Added JUnit 4.12 library. Added initial version of the classes: Assignment, Module, ModulesManager, by incorporating initial version of @BeforeClass, @AfterClass, @Before, @After annotations with methods.

--- UniMarksCalculator/test/ModuleTest.java
+++ UniMarksCalculator/test/ModuleTest.java
@@ -1,13 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
- * @author xxx
+ * @author Lukasz Bol
  */
-public class ModuleTest {
+public class ModuleTest 
+{
+    public ModuleTest(){}
     
+    @BeforeClass
+    public static void setUpClass(){}
+    
+    @AfterClass
+    public static void tearDownClass(){}
+    
+    @Before
+    public void setUp(){}
+    
+    @After
+    public void tearDown(){}    
 }
--- UniMarksCalculator/test/ModulesManagerTest.java
+++ UniMarksCalculator/test/ModulesManagerTest.java
@@ -1,13 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
- * @author xxx
+ * @author Lukasz Bol
  */
-public class ModulesManagerTest {
+public class ModulesManagerTest 
+{
+    public ModulesManagerTest(){}
     
+    @BeforeClass
+    public static void setUpClass(){}
+    
+    @AfterClass
+    public static void tearDownClass(){}
+    
+    @Before
+    public void setUp(){}
+    
+    @After
+    public void tearDown(){}    
 }
