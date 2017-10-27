fd5b52bed37e269de3b4cf1b79c2d7c591dbc57a
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-27 22:38:21 +0100

    Added JOptionPane for ExitHandler for File/Exit menu item.

--- UniMarksCalculator/src/unimarkscalculator/gui/marksGUI.java
+++ UniMarksCalculator/src/unimarkscalculator/gui/marksGUI.java
@@ -142,8 +142,13 @@ public class MarksGUI
     {
 
         @Override
-        public void actionPerformed(ActionEvent e) {
-            System.exit(0);
+        public void actionPerformed(ActionEvent e) 
+        {
+            int answer = JOptionPane.showConfirmDialog(myFrame, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
+            if(answer == JOptionPane.YES_OPTION)
+            {
+                System.exit(0);
+            }
         }
     }
     
