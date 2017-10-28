6cd2d63452b9cc3e7b9e89372c7f7905bfed9feb
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-28 19:31:43 +0100

    Added @Test for getters.

--- UniMarksCalculator/test/unimarkscalculator/mainClasses/AssignmentTest.java
+++ UniMarksCalculator/test/unimarkscalculator/mainClasses/AssignmentTest.java
@@ -2,8 +2,10 @@ package unimarkscalculator.mainClasses;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  *
@@ -11,23 +13,67 @@ import org.junit.BeforeClass;
  */
 public class AssignmentTest 
 {
-    private Assignment testAssignment;
+    private static Assignment testAssignment;
+    
     public AssignmentTest(){}
     
     @BeforeClass
-    public static void setUpClass(){}
+    public static void setUpClass()
+    {
+        testAssignment = new Assignment("AI - NetLogo", "coursework", 85, 50);
+    }
     
     @AfterClass
     public static void tearDownClass(){}
     
     @Before
-    public void setUp()
-    {
-        testAssignment = new Assignment("PEN Test", "coursework", 70, 50);
-    }
+    public void setUp(){}
     
     @After
     public void tearDown(){}
     
+    @Test
+    public void testGetTitle()
+    {
+        String title = "AI - NetLogo";
+        assertEquals(testAssignment.getTitle(), title);
+    }
+    
+    @Test
+    public void testGetType()
+    {
+        String type = "coursework";
+        assertEquals(testAssignment.getType(), type);
+    }
+    
+    @Test
+    public void testGetResult()
+    {
+        double result = 85;
+        assertTrue(testAssignment.getResult() == result);
+        
+    }
+    
+    @Test
+    public void testGetWeight()
+    {
+        double weight = 50;
+        assertTrue(testAssignment.getWeightPercent() == weight);
+    }
+    
+    @Test
+    public void testSetTitle(){}
+    
+    @Test
+    public void testSetType(){}
+    
+    @Test
+    public void testSetResult(){}
+    
+    @Test
+    public void testSetWeight(){}
+    
+
+            
     
 }
