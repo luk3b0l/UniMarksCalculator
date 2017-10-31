474360d650e04b6112875a6ca9d954bf2c46b7b3
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-31 23:46:08 +0000

    Added @Test testGetInstance to test if a new ModulesManager instance has been created properly.

--- UniMarksCalculator/test/unimarkscalculator/mainClasses/ModulesManagerTest.java
+++ UniMarksCalculator/test/unimarkscalculator/mainClasses/ModulesManagerTest.java
@@ -2,8 +2,10 @@ package unimarkscalculator.mainClasses;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  *
@@ -23,5 +25,14 @@ public class ModulesManagerTest
     public void setUp(){}
     
     @After
-    public void tearDown(){}    
+    public void tearDown(){} 
+    
+    @Test
+    public void testGetInstance()
+    {
+        ModulesManager testingModulesManager = ModulesManager.getInstance();
+        String description = "NEW ModulesManager";
+        testingModulesManager.setDescription(description);
+        Assert.assertEquals(description, testingModulesManager.getDescription());
+    }
 }
