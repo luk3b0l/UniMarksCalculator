b6164188fb5c3750d84f55a9d296f6fb34a5bec7
luk3b0l <lukaszbol@yahoo.co.uk>
2017-11-05 23:34:23 +0000

    Added forgotten initial JUnit annotations.

--- UniMarksCalculator/test/unimarkscalculator/gui/ModulesManagerGUITest.java
+++ UniMarksCalculator/test/unimarkscalculator/gui/ModulesManagerGUITest.java
@@ -1,10 +1,25 @@
 package unimarkscalculator.gui;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 /**
  *
  * @author Lukasz Bol
  */
 public class ModulesManagerGUITest 
 {
+    @BeforeClass
+    public static void setUpClass(){}
     
+    @AfterClass
+    public static void tearDownClass(){}
+    
+    @Before
+    public void setUp(){}
+    
+    @After
+    public void tearDown(){}        
 }
